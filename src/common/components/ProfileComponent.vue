64de238e8e71104b33e78a48292c05ee064ea9b1
jong234 <oip9663@naver.com>
2025-10-13 21:43:34 +0900

    프로필 백 화면 파일 업로드 후 적용 작업중

--- src/common/components/ProfileComponent.vue
+++ src/common/components/ProfileComponent.vue
@@ -1,15 +1,47 @@
 <script >
 import HeaderComponent from "@/common/components/HeaderComponent.vue";
 import {aTeamApi} from "@/util/axios";
+
 export default{
   components: {HeaderComponent},
   data(){
     return{
+      profileModal : false,
       profileName: {},
       profileEmail: {},
       profileImg: {},
       backgroundImg: {},
     };
+  },
+  methods: {
+    handleFileChange(event){
+      const file = event.target.files[0];
+      if(file){
+        if (this.backgroundImg) {
+          URL.revokeObjectURL(this.backgroundImg);
+        }
+        this.backgroundImg = URL.createObjectURL(file);
+      } else {
+        this.backgroundImg = null;
+      }
+    },
+    profileModalOpen() {
+      this.profileModal = !this.profileModal
+    },
+    profileModalClose() {
+      if (this.profileModal === true) {
+        this.profileModal = false;
+      }
+    },
+  },
+  watch: {
+    profileModal: function (val) {
+      if (val) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = '';
+      }
+    }
   },
   async mounted() {
     const res = await aTeamApi.get('/api/users/me/profileAll');
@@ -35,7 +67,7 @@ export default{
           ? `${baseUrl}${this.backgroundImg}`
           : "../../assets/AcoountCoverDefault.jpg";
     }
-  }
+  },
 
 }
 
@@ -45,7 +77,8 @@ export default{
   <HeaderComponent/>
   <div id ="accountImgMain">
     <img :src="accountBackImgUrl" id="CoverImg">
-    <button type="button" id = "accountImgUpload"><img src="../../assets/AcoountUploadImgIcon.png"><a>upload new cover</a></button>
+    <button type="button" id = "accountImgUpload" @click="profileModalOpen"><img src="../../assets/AcoountUploadImgIcon.png"><a>upload new cover</a></button>
+
   </div>
   <div id = "accountProfile">
     <div id = "accountProfileMain">
@@ -57,6 +90,23 @@ export default{
         <h3>{{ profileName }}</h3>
         <a>{{ profileEmail }}</a>
       </div>
+    </div>
+  </div>
+  <div class="AccountModalWrap" v-show="profileModal">
+    <div class="ACModalContainer">
+      <!--      취소 버튼-->
+      <img src="../../assets/ModalClose.png" alt="취소 사진" @click="profileModalClose" class="ModalCloseBtn">
+      <h1>파일 업로드</h1>
+      <div>
+        <fieldset class="fieldModal">
+          <legend class="LegendLogin">파 일</legend>
+          <img v-if="previewImg" :src="previewImg" alt="미리보기 사진입니다.">
+          <!--          파일 넣는 곳-->
+          <input type="file"  class="LTextBox">
+        </fieldset>
+      </div>
+      <!--      클릭시 수정 완료-->
+      <button type="button" class="ModalBtnStyle" @change="handleFileChange" accept="image/*" ref="fileInput">업로드</button>
     </div>
   </div>
 
