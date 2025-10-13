53c1e90d994a02c83cb160f3c51e0782e60642a9
TOMHOON777 <125789673+TOMHOON777@users.noreply.github.com>
2025-10-13 17:02:52 +0900

    [Feature] 프론트 배포가이드 md 파일 추가  (#15)
    * feat: 배포 스크립트, 가이드 추가
    
    * fix: 스크립트 빠진 코드 추가
    
    ---------
    
    Co-authored-by: tomhoon <gnsdl9108@naver.com>

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+
+set -e
+
+echo "🥕 최신화"
+git pull | true
+
+echo "🥕 기존 컨테이너 중지"
+docker stop front_container | true
+
+echo "🥕 기존 컨테이너 제거"
+docker rm front_container | true
+
+echo "🥕 기존 이미지 제거"
+docker rmi front_image | true
+
+echo "🥕 프로젝트 빌드"
+npm run build
+
+echo "🥕 신규 이미지 생성"
+docker build -t front_image .
+
+echo "🥕 신규 컨테이너 생성"
+docker run -d -p 80:80 --name front_container --net backend_mjc_network front_image
