d2848ef0199af185b25eb20d52b20183a6717bf3
jong234 <oip9663@naver.com>
2025-10-31 20:09:24 +0900

    fix: 사진 전환 버튼 클릭시 시간초마다 사진이 바뀌는 이벤트와 겹쳐서 두번 이미지 전환되는 문제 수정

--- src/page/LoginPage.vue
+++ src/page/LoginPage.vue
@@ -3,28 +3,30 @@ import eyeoff from '../assets/Eye off.png'
 import eyeon from '../assets/Eye.png'
 import LoginImg1 from '../assets/LoginImg1.jpg'
 import LoginImg2 from '../assets/LoginImg2.jpg'
-import { reactive } from "vue";
+import {reactive} from "vue";
 import {aTeamApi} from "@/util/axios";
 import router from "@/router";
+
 export default {
   data() {
     return {
-      eyeImg : eyeoff,
+      eyeImg: eyeoff,
       loginImg: LoginImg1,
       loginImgBtn1: LoginImg1,
       loginImgBtn2: LoginImg2,
       changeLBtn1: true,
       changeLBtn2: false,
-      timerId : null,
-      viewPassword : 'password',
+      timerId: null,
+      viewPassword: 'password',
     };
   },
   async mounted() {
     this.timerId = setInterval(this.TtoCIMG, 10000);
-
-
   },
-  setup(){
+  beforeUnmount() {
+    clearInterval(this.timerId);
+  },
+  setup() {
     const state = reactive({
       form: {
         userEmail: "",
@@ -42,20 +44,20 @@ export default {
         localStorage.setItem("token", token);
         window.dispatchEvent(new Event("token-changed"));
         await router.push("/");
-      }).catch((error)=> {
+      }).catch((error) => {
         if (error.response?.status === 500) {
           alert("아이디와 비밀번호가 일치 하지 않습니다. 다시 로그인 해주세요.");
-        } else  {
+        } else {
           alert("정보를 가져오는데 실패했습니다.");
         }
       });
     };
-    return { state, submit };
+    return {state, submit};
   },
 
   methods: {
-    changeEyeImg(){
-      if(this.eyeImg === eyeoff){
+    changeEyeImg() {
+      if (this.eyeImg === eyeoff) {
         this.eyeImg = eyeon;
         this.changeInputType();
       } else {
@@ -63,37 +65,42 @@ export default {
         this.changeInputType();
       }
     },
-    changeLoginImg(img){
-      if(img === this.loginImgBtn1){
+    changeLoginImg(img) {
+      if (img === this.loginImgBtn1) {
         this.loginImg = LoginImg1;
-      }else {
+      } else {
         this.loginImg = LoginImg2;
       }
     },
-    changLBtn(btn){
-      if(btn === this.changeLBtn2){
+    changLBtn(btn) {
+      if (btn === this.changeLBtn2) {
         this.changeLBtn2 = true;
         this.changeLBtn1 = false;
-      }else if(btn === this.changeLBtn1){
+        clearInterval(this.timerId);
+        this.timerId = setInterval(this.TtoCIMG, 10000);
+
+      } else if (btn === this.changeLBtn1) {
+        this.changeLBtn1 = true;
+        this.changeLBtn2 = false;
+        clearInterval(this.timerId);
+        this.timerId = setInterval(this.TtoCIMG, 10000);
+      }
+    },
+    TtoCIMG() {
+      if (this.loginImg === LoginImg1) {
+        this.loginImg = LoginImg2;
+        this.changeLBtn2 = true;
+        this.changeLBtn1 = false;
+      } else if (this.loginImg === LoginImg2) {
+        this.loginImg = LoginImg1;
         this.changeLBtn1 = true;
         this.changeLBtn2 = false;
       }
     },
-    TtoCIMG(){
-      if(this.loginImg === LoginImg1){
-        this.loginImg = LoginImg2;
-        this.changeLBtn2 = true;
-        this.changeLBtn1 = false;
-      }else if(this.loginImg === LoginImg2){
-        this.loginImg = LoginImg1;
-        this.changeLBtn1 = true;
-        this.changeLBtn2 = false;
-      }
-    },
-    changeInputType(){
-      if(this.viewPassword === 'password'){
+    changeInputType() {
+      if (this.viewPassword === 'password') {
         this.viewPassword = 'text';
-      }else{
+      } else {
         this.viewPassword = 'password';
       }
     },
@@ -103,7 +110,7 @@ export default {
 </script>
 
 <template>
-  <div id = "LoginMain">
+  <div id="LoginMain">
     <div class="LoginBox">
       <div class="LoginText">
         <h1>Login</h1>
@@ -113,44 +120,54 @@ export default {
 
       <fieldset class="fieldLogin">
         <legend class="LegendLogin">이메일</legend>
-        <input type="email" placeholder="이메일을 입력하세요." value="" class="LTextBox" id="userEmail" v-model="state.form.userEmail">
+        <input type="email" placeholder="이메일을 입력하세요." value="" class="LTextBox" id="userEmail"
+               v-model="state.form.userEmail">
       </fieldset>
       <fieldset class="fieldLogin">
         <legend class="LegendLogin">Password</legend>
-        <input :type="viewPassword" placeholder="비밀번호를 입력하세요." value="" class="LTextBox" id="userPw" v-model="state.form.userPw" autocomplete = "off">
-        <div id = "eye-offBox">
-          <img :src="eyeImg" @click = "changeEyeImg" id ="eye-off" alt="눈 감는 사진">
+        <input :type="viewPassword" placeholder="비밀번호를 입력하세요." value="" class="LTextBox" id="userPw"
+               v-model="state.form.userPw" autocomplete="off">
+        <div id="eye-offBox">
+          <img :src="eyeImg" @click="changeEyeImg" id="eye-off" alt="눈 감는 사진">
         </div>
       </fieldset>
-      <div id = "PwdLine">
+      <div id="PwdLine">
         <span id="LoginCheckboxLine">
-        <input type="checkbox" class ="LoginCheckbox">  비밀번호 기억하기
+        <input type="checkbox" class="LoginCheckbox">  비밀번호 기억하기
         </span>
-        <router-link to="/findpw" class = "FPwd">Forgot Password</router-link>
+        <router-link to="/findpw" class="FPwd">Forgot Password</router-link>
       </div>
       <button @click="submit" id="LoginBtn">Login</button>
-      <div id = "SignUpLink">
+      <div id="SignUpLink">
         <router-link to="/signup" class="SignUpBtn">회원가입</router-link>
       </div>
       <div class="hr-sect">
         Or link with
       </div>
-      <div id ="LoginIconBoxes">
-       <a href="http://localhost:18888/oauth2/authorization/naver"><button type="button" id ="fBtn" class="LBtnGroup"><img src="../assets/Naver.png"></button></a>
-        <a href="http://localhost:18888/oauth2/authorization/google"><button type="button" id = "GBtn" class="LBtnGroup"><img src="../assets/googleLogin.png"></button></a>
-        <a href="http://localhost:18888/oauth2/authorization/kakao"><button type="button" id = "ABtn" class="LBtnGroup" ><img src="../assets/Kakao.png"></button></a>
+      <div id="LoginIconBoxes">
+        <a href="http://localhost:18888/oauth2/authorization/naver">
+          <button type="button" id="fBtn" class="LBtnGroup"><img src="../assets/Naver.png"></button>
+        </a>
+        <a href="http://localhost:18888/oauth2/authorization/google">
+          <button type="button" id="GBtn" class="LBtnGroup"><img src="../assets/googleLogin.png"></button>
+        </a>
+        <a href="http://localhost:18888/oauth2/authorization/kakao">
+          <button type="button" id="ABtn" class="LBtnGroup"><img src="../assets/Kakao.png"></button>
+        </a>
       </div>
     </div>
     <div class="LoginImages">
-      <transition name="fade-in">
-        <img :src="loginImg" style="width: 612px; height: 816px" class="LoginIMG" alt="로그인시 나오는 사진">
+      <transition name="fade">
+        <img :src="loginImg" :key="loginImg" style="width: 612px; height: 816px" class="LoginIMG" alt="로그인시 나오는 사진">
       </transition>
-      <div id = "PicBtnBoxes">
+      <div class="PicBtnBoxes">
          <span style="margin-right: 8px">
-          <button type="button" @click ="changeLoginImg(loginImgBtn1), changLBtn(changeLBtn1)" class="NSelectPicBtn" :class="{'SelectPicBtn': changeLBtn1}"  ></button>
+          <button type="button" @click="changeLoginImg(loginImgBtn1), changLBtn(changeLBtn1)" class="NSelectPicBtn"
+                  :class="{'SelectPicBtn': changeLBtn1}"></button>
          </span>
         <span style="margin-right: 8px">
-          <button type="button" @click="changeLoginImg(loginImgBtn2), changLBtn(changeLBtn2)" class="NSelectPicBtn" :class="{'SelectPicBtn': changeLBtn2}" ></button>
+          <button type="button" @click="changeLoginImg(loginImgBtn2), changLBtn(changeLBtn2)" class="NSelectPicBtn"
+                  :class="{'SelectPicBtn': changeLBtn2}"></button>
          </span>
         <span style="margin-right: 8px">
           <button type="button" class="NSelectPicBtn"></button>
@@ -161,71 +178,98 @@ export default {
 </template>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  position: absolute;
+  transition: opacity 1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+
+  opacity: 0;
+}
+
 * {
-  margin:0;
+  margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-#LoginMain{
+
+#LoginMain {
   display: flex;
   justify-content: center;
 }
-.LoginImages{
+
+.LoginImages {
   display: flex;
+  position: relative;
   justify-content: space-between;
+  overflow: hidden;
   margin: auto auto auto 0;
   width: 616px;
 }
 
-.LoginBox{
-  margin: auto 104px  auto auto;
+.LoginBox {
+  margin: auto 104px auto auto;
   width: 512px;
   height: 593px;
 }
-.LoginText{
+
+.LoginText {
   text-align: left;
   margin-bottom: 48px;
 }
-.PlsL{
+
+.PlsL {
   color: #112211;
 }
 
-.LoginIMG{
+.LoginIMG {
+  display: flex;
   width: 618px;
   height: 816px;
 }
 
-.LegendLogin{
+.LegendLogin {
   text-align: left;
 }
-.fieldLogin{
+
+.fieldLogin {
   display: flex;
   margin-bottom: 24px;
   width: 512px;
   height: 56px;
   border-radius: 4px;
 }
-#PwdLine{
+
+#PwdLine {
   margin-bottom: 40px;
   display: flex;
 }
-#LoginBtn{
+
+#LoginBtn {
   margin-bottom: 16px;
 }
-#SignUpLink{
+
+#SignUpLink {
   margin-bottom: 40px;
 }
-#LoginIconBoxes{
+
+#LoginIconBoxes {
   margin-top: 40px;
   display: flex;
 }
-#fBtn{
+
+#fBtn {
   margin-right: 16px;
 }
-#GBtn{
+
+#GBtn {
   margin-right: 16px;
 }
-.LTextBox{
+
+.LTextBox {
   display: flex;
   flex: 1;
   height: 30px;
@@ -233,38 +277,46 @@ export default {
   margin-left: 16px;
   background-color: white;
 }
-input.LTextBox:focus{
+
+input.LTextBox:focus {
   outline: none;
 }
-#eye-off{
+
+#eye-off {
   width: 24px;
   height: 24px;
 }
-#eye-offBox{
+
+#eye-offBox {
   width: 48px;
   height: 48px;
   float: right;
   display: flex;
   justify-content: center;
 }
-#LoginCheckboxLine{
+
+#LoginCheckboxLine {
   margin-right: 224px;
 }
-.LoginCheckbox{
+
+.LoginCheckbox {
   width: 18px;
   height: 18px;
   border: black solid 2px;
   position: relative;
   top: 3px;
 }
-.FPwd{
+
+.FPwd {
   color: #FF8682;
   text-decoration: none;
 }
-.FPwd:hover{
+
+.FPwd:hover {
   color: #e0605d;
 }
-#LoginBtn{
+
+#LoginBtn {
   width: 100%;
   height: 48px;
   background-color: #8DD3BB;
@@ -273,26 +325,31 @@ input.LTextBox:focus{
   font-weight: bold;
 }
 
-#LoginBtn:hover{
+#LoginBtn:hover {
   background-color: #93efc6;
   color: gray;
 }
-.SignUpBtn{
+
+.SignUpBtn {
   text-decoration: none;
   color: black;
 }
-.SignUpBtn:hover{
+
+.SignUpBtn:hover {
   color: gray;
 }
-.LBtnGroup{
+
+.LBtnGroup {
   border: #8DD3BB solid 1px;
   width: 160px;
   height: 56px;
   background-color: white;
 }
-.LBtnGroup:hover{
+
+.LBtnGroup:hover {
   background-color: #D9D9D9;
 }
+
 .hr-sect {
   display: flex;
   flex-basis: 100%;
@@ -301,6 +358,7 @@ input.LTextBox:focus{
   font-size: 15px;
   margin: 8px 0px;
 }
+
 .hr-sect::before,
 .hr-sect::after {
   content: "";
@@ -311,7 +369,8 @@ input.LTextBox:focus{
   line-height: 0px;
   margin: 0px 16px;
 }
-.NSelectPicBtn{
+
+.NSelectPicBtn {
   display: flex;
   position: relative;
   border: none;
@@ -319,7 +378,8 @@ input.LTextBox:focus{
   height: 10px;
   border-radius: 10px;
 }
-.SelectPicBtn{
+
+.SelectPicBtn {
   display: flex;
   position: relative;
   width: 32px;
@@ -329,14 +389,14 @@ input.LTextBox:focus{
   border: none;
 }
 
-#PicBtnBoxes{
+.PicBtnBoxes {
   display: flex;
   position: relative;
   width: 616px;
   height: 58px;
   right: 332px;
   margin: 758px 0 0 0;
-  justify-content: center;
+  z-index: 10;
 }
 
 </style>
